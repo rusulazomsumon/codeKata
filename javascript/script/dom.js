d8cc2aac5e4ae1779871924ad7f6a32751f292af
RusulAzomSumon <18103020@iubat.edu>
2022-07-21 00:34:09 +0600

    todo design

---
+++ javascript/script/dom.js
@@ -0,0 +1,6 @@
+// Selector in DOM
+// getElementByID
+var nam = document.getElementById("name");
+nam.innerText = "Hlw, RA Sumon Babu";
+nam.style.color = "#e4e434";
+nam.style.backgroundColor = "#000000";

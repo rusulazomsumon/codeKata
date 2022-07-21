c5921f6c1ef5e3d769050ed706aeb382591372a7
Md. Rusul Azom <37295674+rusulazomsumon@users.noreply.github.com>
2022-07-21 16:30:26 +0600

    Update dom.js
    dom change

--- javascript/script/dom.js
+++ javascript/script/dom.js
@@ -1,6 +1,26 @@
 // Selector in DOM
+
+// Examine the documents objects
+// var doc = console.log(document.all);
+// var doc = console.log(document.URL);
+// var doc = console.log(document.title);
+
 // getElementByID
-var nam = document.getElementById("name");
-nam.innerText = "Hlw, RA Sumon Babu";
-nam.style.color = "#e4e434";
-nam.style.backgroundColor = "#000000";
+// Using getElementById after selection we have 
+// change Text, color and background 
+// var heading = document.getElementById("headline");
+// heading.innerHtml = "Keyword Collector";
+// heading.style.color = "#e4e434";
+// heading.style.backgroundColor = "#000000";
+
+// ******GetElementsByClass*******
+// var Keywords = document.getElementsByClassName("list-item");
+// console.log(Keywords); // return collection 
+
+// chane content of 3rd lists item 
+// var Keywords = document.getElementsByClassName("list-item");
+// // Keywords[2].textContent = "ML";
+// //change background of Odd numbers 
+// for(var i=0; i<Keywords.length; i=i+2){
+//     Keywords[i].style.backgroundColor = '#e4e434';
+// }

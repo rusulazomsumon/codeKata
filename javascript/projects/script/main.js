115211ec2fafbe1ce6a8ad670ca0715dbc884a83
Md. Rusul Azom <37295674+rusulazomsumon@users.noreply.github.com>
2022-07-26 15:35:25 +0600

    Update main.js
    js done full code

--- javascript/projects/script/main.js
+++ javascript/projects/script/main.js
@@ -1,16 +1,32 @@
-// var keyVal = document.getElementById("key").value;
-// console.log(keyVal);
+// defination of calculator funtion
+function calculator(keyVal){
+    document.getElementById("screen").value += keyVal;
 
-// getting the button value 
-var keyVal = document.addEventListener("click",getBtnVal);
-//defining the getBtnVal function 
-function getBtnVal(){
-    var btnKeyVal = document.getElementById("key").value;
-    console.log(btnKeyVal);
 }
 
-// showing the value 
-var scrnText = document.getElementById("screen");
-scrnText.innerHTML = keyVal;
+// defining allclear function and ON
+function allClear(){
+    // All clear 
+    document.getElementById("screen").value = " ";
+    // Turn On
+    var screenOn = document.getElementById("screen");
+    screenOn.style.backgroundColor = "#ffffff";    
+}
 
+function answer(){
+    var ans = document.getElementById("screen").value;
+    var calAns = eval(ans);
+    document.getElementById("screen").value = calAns;
+}
+
+// turning off the calculator black window of screen 
+function off(){
+    var screenOff = document.getElementById("screen");
+    screenOff.style.backgroundColor = "#000000";
+}
+
+
+
+
+//advance 
 // link: https://www.youtube.com/watch?v=j59qQ7YWLxw

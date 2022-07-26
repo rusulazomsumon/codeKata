522283b6083fb4ad90d6985babcfca237f1a7d5e
RusulAzomSumon <18103020@iubat.edu>
2022-07-26 20:56:30 +0600

    calculator done

---
+++ javascript/projects/script/main.js
@@ -0,0 +1,30 @@
+// defination of calculator funtion
+function calculator(keyVal){
+document.getElementById("screen").value += keyVal;
+}
+
+// defining allclear function and ON
+    function allClear(){
+   // All clear 
+    document.getElementById("screen").value = " ";
+    // Turn On
+    var screenOn = document.getElementById("screen");
+    screenOn.style.backgroundColor = "#ffffff";    
+}
+
+function answer(){
+    var ans = document.getElementById("screen").value;
+    var calAns = eval(ans);
+    document.getElementById("screen").value = calAns;
+}
+
+
+// turning off the calculator black window of screen 
+function off(){
+    var screenOff = document.getElementById("screen");
+    screenOff.style.backgroundColor = "#000000";
+}
+
+
+
+
